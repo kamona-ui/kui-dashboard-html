413d4213765826e2590ea64fa37280ef7374ded1
Kamona-WD <ak3849909@gmail.com>
2025-01-23 15:11:34 +0200

    add btn helper

--- src/support/hbs-helpers.js
+++ src/support/hbs-helpers.js
@@ -1,3 +1,4 @@
+import { twMerge } from 'tailwind-merge'
 import { baseUrl } from '.'
 
 export default {
@@ -15,5 +16,68 @@ export default {
         }
 
         return `${baseUrl}${url}`
+    },
+
+    btn: function (context) {
+        const {
+            variant = '',
+            icon = null,
+            srText = '',
+            startIcon = null,
+            endIcon = null,
+            className = null,
+            attrs = '',
+            iconClasses = '',
+            iconSizeClasses = 'w-6 h-6',
+            iconAttrs = '',
+            text = '',
+            type = 'button',
+            href = null,
+        } = context.hash
+
+        let baseClasses =
+            'inline-flex items-center justify-center gap-2 rounded-md transition-colors font-medium select-none focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1 disabled:cursor-not-allowed disabled:opacity-50 '
+
+        switch (variant) {
+            case 'black':
+                baseClasses += ' bg-black text-gray-200 hover:bg-gray-900 focus:ring-black'
+                break
+
+            case 'transparent':
+                baseClasses +=
+                    ' bg-transparent text-gray-500 hover:text-gray-600 focus:ring-primary dark:text-gray-300 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-dark-eval-2'
+                break
+
+            default:
+                baseClasses += ' bg-primary text-white hover:bg-primary-dark focus:ring-primary'
+                break
+        }
+
+        if (icon) {
+            baseClasses += ' p-2'
+        } else {
+            baseClasses += ' px-4 py-2'
+        }
+
+        const iconSpan = (icon) =>
+            `<span aria-hidden="true" class="iconify ${icon} ${iconSizeClasses} ${iconClasses}" ${iconAttrs}></span>`
+
+        const srt = srText ? `<span class="sr-only">${srText}</span>` : ''
+        const i = icon ? iconSpan(icon) : ''
+        const si = startIcon ? iconSpan(startIcon) : ''
+        const ei = endIcon ? iconSpan(endIcon) : ''
+
+        const tag = href ? 'a' : 'button'
+
+        return `
+            <${tag} ${href ? 'href="' + href + '"' : ''} ${attrs} type="${type}" class="${twMerge(baseClasses, className)}">
+                ${srt}
+                ${i}
+                ${si}
+                ${text}
+                ${context.fn?.()}
+                ${ei}
+            </${tag}>
+        `
     },
 }
